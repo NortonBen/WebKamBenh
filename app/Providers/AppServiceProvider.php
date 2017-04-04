f23460d29840e799ac4df5922d074ff106fa77d8
ableo <ableo2809@gmail.com>
2017-04-04 17:14:20 +0700

    update

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -15,6 +15,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Schema::defaultStringLength(191);
+        Form::component('bsText', 'components.form.text', ['name', 'value', 'attributes']);
     }
 
     /**
